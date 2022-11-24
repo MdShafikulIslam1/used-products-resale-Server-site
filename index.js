dc721664bbe9387848b035dd56014d6de6c62af9
MdShafikulIslam <179398ssshofik@gmail.com>
2022-11-24 22:28:54 +0600

    category data and allProducts data added to db and loader api is created

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qg5qmf2.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+        const categoriesCollection = client.db('usedProducts').collection('categories');
+        const allProductsCollection = client.db('usedProducts').collection('allProducts')
+        //all categories loadded api 
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const result = await categoriesCollection.find(query).toArray();
+            res.send(result);
+        });
+        //useing id loaded specific categories data for API
+        app.get('/categories/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { category_id: id }
+            const result = await allProductsCollection.find(query).toArray();
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.log(err))
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Used resale products server site is runnig')
+});
+
+app.listen(port, () => {
+    console.log(`used products server site is runnig on this port:${port}`);
+});
